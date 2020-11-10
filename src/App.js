db4bda13ebf960806a6d7a1aa7a74a9dff453202
Sergey Biryukov <2700036@mail.ru>
2020-11-10 23:22:11 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import TimerSlot from './TimerSlot';
+import { useSpeechSynthesis } from 'react-speech-kit';
+
+import './App.css';
+import { useStopwatch } from 'react-timer-hook';
+
+export default function App() {
+  const [timers, setTimers] = useState([
+    {time: 1, text: 'Привет!', isActive: false},
+    {time: 3, text: 'Ты можешь написать что-нибудь', isActive: false},
+    {time: 6, text: 'И я просто скажу это', isActive: false},
+  ]);
+  const { speak, speaking, supported, voices } = useSpeechSynthesis();
+  const {
+    seconds, isRunning, start, reset
+  } = useStopwatch();
+
+  const updateTimer = (i, time, text, isActive = false) => {    
+    setTimers([...timers.slice(0, i), 
+    {time, text, isActive}, ...timers.slice(i+1,)])
+  }
+  const addTimerSlot = () => {
+    setTimers([...timers, {time: Math.floor(timers[timers.length-1].time 
+      +timers[timers.length-1].text.length/6), text: 'Cообщение', isActive: false}])
+  }
+
+  const resetTalk = () => {
+    const newTimers = timers.map((timer) =>{
+      timer.isActive = false        
+      return timer
+    })  
+    setTimers(newTimers);
+    reset();
+  }
+
+  useEffect(() => {
+    const foundTimer = timers.find(({time}) => time == seconds)
+    if (foundTimer) {    
+      const ind = timers.indexOf(foundTimer);
+      const newTimers = timers.map((timer, i, arr) =>{
+        arr[i] == arr[ind] ? timer.isActive = true : timer.isActive = false        
+        return timer
+      })  
+      setTimers(newTimers);
+      speak({text: foundTimer.text, voice: voices[18]})
+    }
+    if (seconds > timers[timers.length-1].time+1) resetTalk();
+  }, [seconds])
+
+
+
+  return (
+    <div className="app">
+      <h2>Разговоры c браузером</h2>
+
+      {supported && 
+      <>
+      <div className="timers">
+        {timers.map((timer, index)=>
+        <TimerSlot 
+        key={index}
+        timer={timer} 
+        updateTimer={(time, text)=>updateTimer(index, time, text)}
+        
+        />)
+        }
+
+        <button className="add-button" onClick={addTimerSlot}>Add</button>
+      </div>
+
+      {/* seconds */}
+      <h2>{seconds}</h2>
+
+      {/* buttons */}
+      <div className="buttons">
+        {!isRunning && <button className="start-button" onClick={start}>
+          Start
+          </button>}
+        {isRunning && <button className="stop-button" onClick={resetTalk}>
+          Stop
+          </button>}
+      </div> 
+      </>}
+      {!supported && <div>Ваш браузер не поддерживается приложением.</div>}
+    </div>
+  );
+}
---
+++ src/TimerSlot.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export default function TimerSlot({timer, updateTimer}) {
+  const [time, setTime] = useState(timer.time);
+  const [text, setText] = useState(timer.text);
+  console.log(timer)
+  const handleBlur = () => {
+    updateTimer(time, text)
+  }
+  return (
+    <form className="timer">
+          <input className={`${timer.isActive ? 'input_active' : ''}`}
+          type="number" value={time} 
+          onChange={({target})=>setTime(target.value)}
+          onBlur={handleBlur}
+          />
+          <input className={`${timer.isActive ? 'input_active' : ''}`}
+          type="text" value={text} 
+          onChange={({target})=>setText(target.value)}
+          onBlur={handleBlur}
+          />
+        </form>
+  )
+}
+
+
