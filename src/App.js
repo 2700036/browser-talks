3511bf5aed6a2d245e2e4b59fd6a48f88af62d73
Sergey Biryukov <2700036@mail.ru>
2020-11-16 23:53:52 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -4,56 +4,85 @@ import { useSpeechSynthesis } from 'react-speech-kit';
 
 import './App.css';
 import { useStopwatch } from 'react-timer-hook';
+import { randomPrase } from './utils';
+
+
 
 export default function App() {
   const [timers, setTimers] = useState([
-    {time: 1, text: 'Привет!', isActive: false},
-    {time: 3, text: 'Ты можешь написать что-нибудь', isActive: false},
-    {time: 6, text: 'И я просто скажу это', isActive: false},
-    // {time: 9, text: 'Слева - секунда на которой будет произнесена фраза', isActive: false},
+    {id: 1, text: 'Привет!', placeholder: 'Сообщение', isActive: false},
+    {id: 2, text: 'Ты можешь написать что-нибудь', placeholder: 'Сообщение', isActive: false},
+    {id: 3, text: 'И я просто скажу это', placeholder: 'Сообщение', isActive: false},
+    
   ]);
+  const [currentPhrase, setCurrentPhrase] = useState({id: 0, text: ' ', placeholder: 'Сообщение', isActive: false});
   const { speak, speaking, supported, voices } = useSpeechSynthesis();
   const {
-    seconds, isRunning, start, reset
+    isRunning, start, reset
   } = useStopwatch();
 
-  const updateTimer = (i, time, text, isActive = false) => {    
-    setTimers([...timers.slice(0, i), 
-    {time, text, isActive}, ...timers.slice(i+1,)])
-  }
-  const addTimerSlot = () => {
-    setTimers([...timers, {time: Math.floor(timers[timers.length-1]?.time 
-      + (timers[timers.length-1]?.text.length>25
-        ?timers[timers.length-1]?.text.length/10 
-        :timers[timers.length-1]?.text.length/6)
-        ) || 1, text: 'Cообщение', isActive: false}])
+  function setNextTimer (){
+    setCurrentPhrase((currentPhrase)=>{
+      const i = timers.findIndex(el=>el.id === currentPhrase.id)      
+      if(!timers[i+1])resetTalk();        
+      return timers[i+1]}); 
+  
+      setTimers(timers.map((timer) =>{
+        timer.id === currentPhrase.id ? timer.isActive = true : timer.isActive = false        
+        return timer
+      }));
   }
 
-  const resetTalk = () => {
-    const newTimers = timers.map((timer) =>{
-      timer.isActive = false        
-      return timer
-    })  
-    setTimers(newTimers);
+  const resetTalk = () => {    
+    setTimeout(() => {
+      const newTimers = timers.map((timer) =>{
+        timer.isActive = false        
+        return timer
+      })  
+      setTimers(newTimers);      
+    }, 2000);    
+    
     reset();
+    setCurrentPhrase({id: 0, text: ' ', placeholder: 'Сообщение', isActive: false});
+  }
+
+  const startTalk = () => {
+    if(currentPhrase.id == 0 ){
+      speak({text: currentPhrase.text, voice: voices[18]})
+      setCurrentPhrase(timers[0])
+      start()      
+      return
+    }
+    if(currentPhrase.text.length < 1){
+      speak({text: randomPrase(), voice: voices[18]})
+      setNextTimer ()
+      return
+    }
+    if(speaking)return;
+    start()            
+    setTimeout(() => {       
+      speak({text: currentPhrase.text, voice: voices[18]})
+    }, 700)
+  
+    setNextTimer ()
+
+  };
+  
+    const updateTimer = (i, text) => {      
+      const newTimers = timers.map(timer => timer.id == i ? {...timer, text, isActive: false, placeholder: 'Сообщение'} : timer)   
+      setTimers(newTimers);
+     
   }
 
   useEffect(() => {
-    const foundTimer = timers.find(({time}) => time == seconds)
-    if (foundTimer) {    
-      const ind = timers.indexOf(foundTimer);
-      const newTimers = timers.map((timer, i, arr) =>{
-        arr[i] == arr[ind] ? timer.isActive = true : timer.isActive = false        
-        return timer
-      })  
-      setTimers(newTimers);
-      speak({text: foundTimer.text, voice: voices[18]})
-    }
-    console.log(seconds, timers[timers.length-1]?.time+1)
-    if (seconds > timers[timers.length-1]?.time+1) resetTalk();
-  }, [seconds])
+    if(!isRunning || speaking)return; 
+           
+    startTalk();
+  }, [speaking])
 
-
+  const addTimerSlot = () => {
+    setTimers([...timers, {id: Math.floor(Math.random() * 1000000), text: '', placeholder: 'Сообщение', isActive: false}])
+  }
 
   return (
     <div className="app">
@@ -62,27 +91,25 @@ export default function App() {
       {supported && 
       <>
       <div className="timers">
-        {timers.map((timer, index)=>
+        {timers.map((timer)=>
         <TimerSlot 
-        key={index}
+        key={timer.id}
         timer={timer} 
-        updateTimer={(time, text)=>updateTimer(index, time, text)}
+        updateTimer={(text)=>updateTimer(timer.id, text)}
         
         />)
         }
 
         <button className="add-button" onClick={addTimerSlot}>Добавить реплику +</button>
       </div>
-
-     
-      {/* <h2>{seconds}</h2> */}
-
       
       <div className="buttons">
-        {!isRunning && <button className="start-button" onClick={start}>
+        {!isRunning && <button className="start-button" onClick={startTalk}>
           Говори!
           </button>}
-        {isRunning && <button className="stop-button" onClick={resetTalk}>
+        {isRunning && <button className="stop-button" 
+        onClick={resetTalk}
+        >
           Прекрати
           </button>}
       </div> 
--- src/TimerSlot.js
+++ src/TimerSlot.js
@@ -1,23 +1,19 @@
 import React, { useState } from 'react'
 
-export default function TimerSlot({timer, updateTimer}) {
-  const [time, setTime] = useState(timer.time);
+export default function TimerSlot({timer, updateTimer}) {  
   const [text, setText] = useState(timer.text);
-  console.log(timer)
-  const handleBlur = () => {
-    updateTimer(time, text)
+  
+  const handleBlur = (e) => {
+    e.preventDefault();   
+    updateTimer(text)
   }
   return (
-    <form className="timer">
-          {/* <input className={`${timer.isActive ? 'input_active' : ''}`}
-          type="number" value={time} 
-          onChange={({target})=>setTime(target.value)}
-          onBlur={handleBlur}
-          /> */}
+    <form className="timer" onSubmit={handleBlur}>          
           <input className={`${timer.isActive ? 'input_active' : ''}`}
-          type="text" value={text} 
+          type="text" value={text} placeholder={timer.placeholder}
           onChange={({target})=>setText(target.value)}
           onBlur={handleBlur}
+          
           />
         </form>
   )
